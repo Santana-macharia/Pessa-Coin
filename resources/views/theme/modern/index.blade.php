2b51f309c27c586009399083c3c9705e346fe0c9
Santana-macharia <santanamash@gmail.com>
2020-10-12 15:29:13 +0300

    improved homepage

--- resources/views/theme/modern/index.blade.php
+++ resources/views/theme/modern/index.blade.php
@@ -23,6 +23,7 @@
                     </div>
                     <div class="clearfix"></div>
                  </div>
+                 
 
             
 
@@ -30,11 +31,146 @@
             </div>
         </div>
         <br><br><br><br><br>
+        <!-- About Us -->
     </div>
+    <h1 style="text-align:center;font-weight:bold;color:black" >ABOUT US</h1>
+    <p style="text-align:center;color:black">51 Capital is a platform for the next generation of investors exploring how cryptocurrencies and digital assets are contributing to the evolution of the global financial system. Its mandate is to inform, educate, and connect the global investment community through news, data, events and education.</p>
+    <h1 style="text-align:left;font-weight:bold;color:black" >51CAPITAL PESSACOIN</h1>
+    
+<ul style="color:black; display:inline-block">
+<li>100 PessaToken will be equivalent of 1 pessacoin</li>
+<li>PessaToken will work to the advantage of stakeholders.</li>
+<li>PessaToken will support funding Binary accounts.</li>
+<li>PessaToken will be affordable with benchmark rate of $0.5 per Token.</li>
+<li>PessaToken will be friendly among other platform, Mpesa.</li>
+<ul><a href="" class="btn btn-info btn-lg" style="margin-top:20px">DISCOVER MORE</a></ul>
+</ul>
 
-   
+<!-- image -->
+<img class="avia_image" src="https://51capitalcrypto.com/old1/wp-content/uploads/2020/06/faviconpesacoin-287x300.png" alt="" title="pessacoin" height="250" width="250" itemprop="thumbnailUrl" style="margin-left:350px;margin-top:-150px">
+
+<!-- Trading with pessacoin div -->
+<div class="container" style="background-color:#000000;width:100%">
+
+<section class="av_textblock_section " itemscope="itemscope" itemtype="https://schema.org/CreativeWork">
+<div class="avia_textblock  av_inherit_color " style="color:#ffffff; " itemprop="text">
+<h2 style="text-align: center;">Why one needs to trade with PessaCoin</h2>
+<h3 style="text-align: center;">There are endless reasons to secure PessaCoins but in a nutshell, everyone seeks:</h3>
+</div></section>
+
+<!-- Trading with pessacoin div -->
+<style>
+* {
+  box-sizing: border-box;
+}
+
+/* Create three equal columns that floats next to each other */
+.column {
+  float: left;
+  width: 33.33%;
+  padding: 10px;
+  height: 300px; /* Should be removed. Only for demonstration */
+}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+</style>
+
+<div class="row">
+  <div class="column" style="background-color:#2c3e50;color:black;font-size:19px;text-align:center;color:#ffffff">
+    <h2>Affordability</h2>
+    <p>The Coins can be bought at the least benchmark rate, which is $0.5 per token. 100 token will be equivalent to 1 PessaCoin. cheaper than the ever-changing Bitcoin rate that has been in use all along for Binary.</p>
+  </div>
+  <div class="column" style="background-color:#2c3e50;color:black;font-size:19px;text-align:center;color:#ffffff">
+    <h2>Profitability</h2>
+    <p>Pesacoin trading will be like any other crypto trading. Exchange rates will be available online and one can monitor pricing and trade accordigly. This market will be totally depending on demand and supply basis.</p>
+  </div>
+  <div class="column" style="background-color:#2c3e50;color:black;font-size:19px;text-align:center;color:#ffffff">
+    <h2>Efficiency</h2>
+    <p>The PesaCoin is efficient in that it can trade with all other currencies from Ksh, $, Bitcoin and Ethereum to list but a few.</p>
+  </div>
+</div>
+<!-- end div -->
+
+</div>
+
+
+
+<div class="avia_textblock  " style="font-size:18px;color:black " itemprop="text"><h2 style="text-align: center;">How to Get Started</h2>
+<p style="text-align: center;color:black">The trade process from the creation of the user account to having pesscoins in your wallet takes only a few minutes.</p>
+</div>
+
+<!-- How to get started div -->
+<style>
+* {
+  box-sizing: border-box;
+}
+
+/* Create three equal columns that floats next to each other */
+.column {
+  float: left;
+  width: 33.33%;
+  padding: 10px;
+  height: 300px; /* Should be removed. Only for demonstration */
+}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+</style>
+
+<div class="row" style="margin-top:-2px">
+  <div class="column" style="background-color:#ffffff;color:#775300;font-size:18px;text-align:center">
+    <h2>CREATE ACCOUNT</h2>
+    <p> Register to 51capitalcrypto.com and  gain access to be a member</p>
+  </div>
+  <div class="column" style="background-color:#ffffff;color:#775300;font-size:18px;text-align:center">
+    <h2>BUY PESSATOKENS</h2>
+    <p>Make deposit amount to your wallet, its simple and secure</p>
+  </div>
+  <div class="column" style="background-color:#ffffff;color:#775300;font-size:18px;text-align:center">
+    <h2>EXCHANGE PESSATOKENS</h2>
+    <p>Earn 1% of your deposit amount each day. Exchange token to other currencies.</p>
+  </div>
+</div>
+<!-- end div -->
+
+
+<!-- Sign in-->
+<div style="background-color:#775300">
+<div class="flex_column av_one_half  flex_column_div first  avia-builder-el-35  el_before_av_one_half  avia-builder-el-first" 
+style="border-width:1px; border-style:solid;  box-shadow: 5px 5px 5px 5px #ffffff; padding:5px; border-radius:10px;width: 400px;height:147px;display:inline-block;margin-left: 200px;margin-top: 100px;margin-bottom:100px ">
+<section class="av_textblock_section " itemscope="itemscope" itemtype="https://schema.org/CreativeWork">
+<div class="avia_textblock  " style="font-size:18px;color:#ffffff " itemprop="text">
+<h2 style="text-align: center;">Sign In</h2>
+<p style="text-align: center;">Access the cryptocurrency you deserve</p>
+</div></section>
+<div class="avia-button-wrap avia-button-center  avia-builder-el-37  el_after_av_textblock  avia-builder-el-last " style="text-align:center">
+<a href="/wallet/user/create" class="avia-button   avia-icon_select-no avia-color-theme-color avia-size-large avia-position-center" style="font-size: 24px;color:#ffffff">
+<span class="avia_iconbox_title">SIGN IN</span></a></div></div>
+
+ <!-- register -->
+<div class="flex_column av_one_half  flex_column_div first  avia-builder-el-35  el_before_av_one_half  avia-builder-el-first"
+style="border-width:1px; border-style:solid;  box-shadow: 5px 5px 5px 5px #ffffff; padding:10px; border-radius:10px;width: 400px;height:150px;display:inline-block;margin-left: 150px;color:#ffffff ">
+<section class="av_textblock_section " itemscope="itemscope" itemtype="https://schema.org/CreativeWork">
+<div class="avia_textblock  " style="font-size:18px; " itemprop="text">
+<h2 style="text-align: center;">Register</h2>
+<p style="text-align: center;">Join the new era of cryptocurrency exchange</p>
+</div></section>
+<div class="avia-button-wrap avia-button-center  avia-builder-el-37  el_after_av_textblock  avia-builder-el-last" style="text-align:center">
+<a href="/wallet/user/create" class="avia-button   avia-icon_select-no avia-color-theme-color avia-size-large avia-position-center" style="font-size: 24px;color:#ffffff;">
+<span class="avia_iconbox_title">SIGN UP</span></a></div></div>
+</div>
 
 @endsection
+
 
 @section('page-js')
     <script>
